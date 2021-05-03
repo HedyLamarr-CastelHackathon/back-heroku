ac2b73c8d2f48e374ddf7547ba5d11dc829d88bb
CYRIL VASSALLO <42696124+cyril-vassallo@users.noreply.github.com>
2021-05-03 23:45:41 +0200

    add fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,7 +11,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
-        $user = new User();
+     
 
         $users = [
             [
@@ -22,7 +22,8 @@ class AppFixtures extends Fixture
             'job' => 'développeur web backend',
             'description' => 'Développeur Fullstack php/javascript. Je participe Hakathon en temps que développeur backend dans l\'équipe HedyLamarr',
             'email' =>  'cyrilvssll34@gmail.com',
-            'birthday' => '19 Juillet'
+            'birthday' => '19 Juillet',
+            'gitHub' => 'https://github.com/cyril-vassallo'
             ],
             [
             'role' => 'ROLE_ADMIN',
@@ -32,7 +33,8 @@ class AppFixtures extends Fixture
             'job' => 'développeur web backend',
             'description' => 'J\'exel dans le développement de site de commerce. Je serait le second développeur backend de l\'équipe',
             'email' =>  'younes-b@gmail.com',
-            'birthday' => '14 avril'
+            'birthday' => '14 avril',
+            'gitHub' => ''
             ],
             [
             'role' => 'ROLE_ADMIN',
@@ -40,9 +42,10 @@ class AppFixtures extends Fixture
             'lastname' => ' Cloquet',
             'age' => 24,
             'job' => 'développeur web frontend',
-            'description' => 'Ex développeur de jeux vidéo Chez Ubi, Je me spécialise en Javascript. Je serais le lead développer  React de l\'équipe HedyLamarr',
+            'description' => 'Ex développeur de jeux vidéo Chez Ubi. Je me spécialise en Javascript. Je serai le lead développer React de l\'équipe HedyLamarr',
             'email' =>  'ben-cloquet@gmail.com',
-            'birthday' => '24 Janvier'
+            'birthday' => '24 Janvier',
+            'gitHub' => ''
             ],
             [
             'role' => 'ROLE_ADMIN',
@@ -50,22 +53,24 @@ class AppFixtures extends Fixture
             'lastname' => ' Landrieux',
             'age' => 28,
             'job' => 'développeur web frontend',
-            'description' => 'Je débute en React mais pas en Js. Je renforcerais notre lead dev front durant l',
+            'description' => 'Je débute en React mais pas en Js. Je viens renforcer notre lead front',
             'email' =>  'ben-cloquet@gmail.com',
-            'birthday' => '24 Janvier'
+            'birthday' => '24 Janvier',
+            'gitHub' => ''
             ]
         ];
 
-        for($u = 0; $u < count($users) ;  $u++ ){
-            $user->setFirstname($users[$u]['firstname'])
-                 ->setLastname($users[$u]['lastname'])
-                 ->setAge($users[$u]['age'])
-                 ->setJob($users[$u]['job'])
-                 ->setDescription($users[$u]['description'])
-                 ->setEmail($users[$u]['email'])
-                 ->setBirthday($users[$u]['birthday'])
-                 ->setRole($users[$u]['role'])
-                 ->setGitHub($users[$u]['role']);
+        foreach($users as $key  => $value ){
+            $user = new User();
+            $user->setFirstname($users[$key]['firstname'])
+                 ->setLastname($users[$key]['lastname'])
+                 ->setAge($users[$key]['age'])
+                 ->setJob($users[$key]['job'])
+                 ->setDescription($users[$key]['description'])
+                 ->setEmail($users[$key]['email'])
+                 ->setBirthday($users[$key]['birthday'])
+                 ->setRole($users[$key]['role'])
+                 ->setGitHub($users[$key]['gitHub']);   
             $manager->persist($user);
         }
         $manager->flush();
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -42,7 +42,7 @@ class User
 
     /**
      * @ORM\Column(type="text", nullable=true)
-     */
+     */ 
     private $description;
 
     /**
